82e2011585f3e3b7418e2151e1d2876e4497ab31
Hamza <hamzabitup@gmail.com>
2020-04-21 14:33:32 +0000

    Feat: dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
+import { ReactComponent as BellIcon } from './icons/bell.svg';
+import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as PlusIcon } from './icons/plus.svg';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
+import { ReactComponent as BoltIcon } from './icons/bolt.svg';
 
 function App() {
   return (
     <Navbar>
-      <NavItem icon="asdf " />
-      <NavItem icon="asdf " />
-      <NavItem icon="asdf " />
+      <NavItem icon={<PlusIcon />} />
+      <NavItem icon={<BellIcon />} />
+      <NavItem icon={<MessengerIcon />} />
+
+      <NavItem icon={<CaretIcon />} >
+        {/* dropdown goes here  */}
+        <DropdownMenu />
+      </NavItem>
     </Navbar>
   );
 }
@@ -14,17 +27,48 @@ function App() {
 function Navbar(props) {
   return (
     <nav className="navbar">
-      <ul className="navbar-nav"> { props.children }</ul>
+      <ul className="navbar-nav"> {props.children}</ul>
     </nav>
   );
 }
 
+function DropdownMenu() {
+
+  function DropdownItem(props) {
+    return (
+      <div className="menu-item">
+        <span className="icon-button">{ props.leftIcon }</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </div>
+    );
+  }
+
+  return (
+   <div className="dropdown">
+      <DropdownItem
+      >
+        My profile
+      </DropdownItem>
+      <DropdownItem
+        leftIcon={<CogIcon />}
+        rightIcon={<ChevronIcon />} 
+      >
+
+      </DropdownItem>
+    </div>
+  );
+}
+
 function NavItem(props) {
+  const [open, setOpen] = useState();
+
   return (
     <li className="nav-item">
-      <div href="#" className="icon-button">
-        { props.icon }
+      <div href="#" className="icon-button" onClick={() => setOpen(!open)}>
+        {props.icon}
       </div>
+      {open && props.children}
     </li>
   );
 }
